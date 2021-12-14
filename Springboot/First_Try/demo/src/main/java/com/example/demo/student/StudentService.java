9e18da89873651a717c2cea0cfa769c4fb5a5ccb
FD3S-Kyle <47182804+FD3S-Kyle@users.noreply.github.com>
2021-12-13 21:06:33 -0400

    Made it just before adding post

---
+++ Springboot/First_Try/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+// import java.time.*;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+	private final StudentRepository studentRepository;
+
+	@Autowired
+    public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+	public List<Student> getStudents()
+	{
+		return studentRepository.findAll();
+	}
+
+    public void addNewStudent(Student student) 
+	{
+		System.out.println(student);
+    }
+}
