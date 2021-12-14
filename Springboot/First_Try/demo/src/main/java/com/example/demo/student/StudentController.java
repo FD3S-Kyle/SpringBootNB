bd90fd78f07f84c14967125e0aca85e56a1e669c
FD3S-Kyle <47182804+FD3S-Kyle@users.noreply.github.com>
2021-12-14 12:15:51 -0400

    Finished the demo

--- Springboot/First_Try/demo/src/main/java/com/example/demo/student/StudentController.java
+++ Springboot/First_Try/demo/src/main/java/com/example/demo/student/StudentController.java
@@ -3,10 +3,14 @@ package com.example.demo.student;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,15 +25,31 @@ public class StudentController {
         this.studentService = studentService;
     }
     
-    @GetMapping
+    @GetMapping //GET aspect
 	public List<Student> getStudents()
 	{
 		return studentService.getStudents();
 	}
 
-    @PostMapping
+    @PostMapping //POST aspect
     public void registerNewStudent(@RequestBody Student student) //takes the request body and maps it to a student
     {
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentID}") //DELETE aspect via a route 
+    public void deleteStudent(@PathVariable("studentID") Long studentID)
+    {
+        studentService.deleteStudent(studentID);
+    }
+
+    @PutMapping(path = "{studentID}")
+    public void updateStudent(
+                    @PathVariable("studentID") Long studentID,
+                    @RequestParam(required = false) String name,
+                    @RequestParam(required = false) String email)
+    {
+        studentService.updateStudent(studentID, name, email);
+    }
+
 }
--- Springboot/First_Try/demo/src/main/java/com/example/demo/student/StudentService.java
+++ Springboot/First_Try/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,8 @@ package com.example.demo.student;
 // import java.time.*;
 import java.util.*;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +20,55 @@ public class StudentService {
 
 	public List<Student> getStudents()
 	{
-		return studentRepository.findAll();
+		return studentRepository.findAll(); //finds all the students
 	}
 
     public void addNewStudent(Student student) 
 	{
-		System.out.println(student);
+		Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail()); //finds email
+		
+		if(studentOptional.isPresent())  //checks if the email is present
+		{
+			throw new IllegalStateException("email taken"); //if an email is present then throw this exception
+		}
+
+		studentRepository.save(student); //save student email
+    }
+
+	public void deleteStudent(Long studentID) //delete method
+	{
+		boolean exists = studentRepository.existsById(studentID); //checks if student exist by their id
+		
+		if(!exists)
+		{
+			throw new IllegalStateException("student with id " + studentID + " does not exists"); //throws exception if the id does not exists
+		}
+
+		studentRepository.deleteById(studentID); //delete student via their id
+	}
+
+	@Transactional //this annotation this allows for values within the db to be updated without queries
+    public void updateStudent(Long studentID, String name, String email) 
+	{
+		Student student = studentRepository.findById(studentID).orElseThrow(() -> new IllegalStateException("student with id " + studentID + " does not exist"));
+
+		if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name))
+		{
+			student.setName(name);
+		}
+
+		if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email))
+		{
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email); //find students email
+
+			if(studentOptional.isPresent()) //checks if email is taken, if so throw exception
+			{
+				throw new IllegalStateException("email taken");
+			}
+
+			student.setEmail(email); //update email via the setter function
+		}
+
+
     }
 }
